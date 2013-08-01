76ac495e8455d7cbf48694161e44ead599849b50
yangli907 <yangli907@gmail.com>
2013-08-01 00:39:43 -0700

    updated local config

--- eclipsify_application_win.bat
+++ eclipsify_application_win.bat
@@ -3,6 +3,6 @@ echo ****************************
 echo Go to your browser for http://localhost:9000 to run the program.
 echo Open Eclipse, choose import: import existing project
 echo ****************************
-rmdir rop\tmp /s /q
-play-1.2.4\play eclipsify rop
+rmdir cves\tmp /s /q
+play-1.2.4\play eclipsify cves
 pause > nul
