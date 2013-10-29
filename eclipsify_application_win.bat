21edded1ac6851af3548c241d120e67b8f79adbe
yangli907 <yangli907@gmail.com>
2013-10-28 23:34:13 -0700

    Update eclipsify_application_win.bat

--- eclipsify_application_win.bat
+++ eclipsify_application_win.bat
@@ -1,7 +1,7 @@
 @echo off
 echo ****************************
 echo Go to your browser for http://localhost:9000 to run the program.
-echo Open Eclipse, choose import: import existing project
+echo Open Eclipse, choose import: import existing project.
 echo ****************************
 rmdir cves\tmp /s /q
 play-1.2.4\play eclipsify cves
